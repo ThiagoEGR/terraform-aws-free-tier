58783d614aff876804cb6f39b0385910db0a565c
thiagoegr <tegr@ic.ufal.br>
2026-06-09 12:23:02 -0300

    feat: configure VPC using terraform-aws-vpc module

---
+++ network.tf
@@ -0,0 +1,13 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
+
+  name = "example-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
+
+  enable_dns_hostnames    = true
+}
