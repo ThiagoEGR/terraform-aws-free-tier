98233e44cabea342bf3576e1bcc962705d982cf9
thiagoegr <tegr@ic.ufal.br>
2026-06-09 21:44:30 -0300

    feat: add security groups and group ingress rules

--- network.tf
+++ network.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.19.0"
 
-  name = "example-vpc"
+  name = "aws-lab-vpc"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b"]
@@ -10,4 +10,51 @@ module "vpc" {
   private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
 
   enable_dns_hostnames    = true
+  enable_nat_gateway = false
 }
+
+resource "aws_security_group" "web_app" {
+  name        = "web_app_sg"
+  description = "Allow inbound HTTP e HTTPS traffic"
+  vpc_id      = module.vpc.vpc_id
+  tags = {
+    Name = "web_app_sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = aws_security_group.web_app.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https" {
+  security_group_id = aws_security_group.web_app.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
+}
+
+resource "aws_security_group" "rds" {
+  name        = "rds_sg"
+  description = "Allow inbound PostgreSQL traffic from web app security group"
+  vpc_id      = module.vpc.vpc_id
+  tags = {
+    Name = "rds_sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "postgresql" {
+  security_group_id = aws_security_group.rds.id
+
+  referenced_security_group_id = aws_security_group.web_app.id
+  from_port               = 5432
+  ip_protocol             = "tcp"
+  to_port                 = 5432
+}
+
